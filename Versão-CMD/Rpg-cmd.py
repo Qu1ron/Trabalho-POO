5e7395102ceff7aefbe307def8a7b95e0ee32f9c
Qu1ron <lucas.monteiro02@aluno.ifce.edu.br>
2025-06-24 19:12:09 -0300

    Corrigido alguns bugs e erros de logica
    vários self. estavam faltando e a logica da função dodge estava invertida

--- Versão-CMD/Rpg-cmd.py
+++ Versão-CMD/Rpg-cmd.py
@@ -18,7 +18,7 @@ class Personagem:
         #Funcao pro ataque basico
     def ataque(self):
         self.dmg = 10
-        return dmg
+        return self.dmg
         
         #Funcao pra checar se o Personagem esta vivo
     def survived(self):
@@ -29,23 +29,20 @@ class Personagem:
     
         #funcao pra calcular o dano
     def damage_cal(self,dano):
-        Hp = Hp - (dano-self.Defense)
+        self.Hp = self.Hp - (dano-self.Defense)
         
     
         #funcao pro dodge, tem que testar pra ver se a formula funciona bem   
     def dodge(self):
         self.base_chance = min(0.5*self.speed, 90) #chance basica, sem nenhuma alteracao 50%, o min limita a 90% a chance base
         self.sorte = random.randint(0,10) #gera um numero aleatorio [0, 10]
-        self.chance_desvio = base_chance + sorte #Entao se voce tem 100 de speed, 50 de base chance + de 0 a 10 dependendo da sorte
+        self.chance_desvio = self.base_chance + self.sorte #Entao se voce tem 100 de speed, 50 de base chance + de 0 a 10 dependendo da sorte
         self.rolagem = random.randint(0,100) #pensa num dado de 100 lados, se o numero que sair for menor que a chance, vc toma :P
-        if chance_desvio < rolagem:
-            return False
-        else: 
+        if self.chance_desvio < self.rolagem:
             return True
+        else: 
+            return False
         
-
-
-
         
         
         
