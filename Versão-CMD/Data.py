1c714ec79cffd220b628af3bd523356b9e6a3796
Lidrac <pedrohm3103@gmail.com>
2025-06-24 22:11:10 -0300

    Ataques do guerreiro

--- Versão-CMD/Data.py
+++ Versão-CMD/Data.py
@@ -1,9 +1,9 @@
-
-
-
 Ataques = {
     'Fireball': {'Damage': 25, 'Mp': 10, 'Classe': 'Mago', 'Efeito': 'burn'},
     'Ice Spike': {'Damage': 20, 'Mp': 30, 'Classe': 'Mago', 'Efeito': 'freeze'},
     'Thunderbolt': {'Damage': 30, 'Mp': 30, 'Classe': 'Mago', 'Efeito': 'shocked'},
     'Hydro Pump': {'Damage': 28, 'Mp': 20, 'Classe': 'Mago', 'Efeito': 'soaked'},
+    'Slash': {'Damage': 28, 'Mp': 20, 'Classe': 'Guerreiro', 'Efeito': 'bleed'},
+    'Blade of Fate': {'Damage': 50, 'Mp': 40, 'Classe': 'Guerreiro', 'Efeito': 'Não sei'},
+    'Last Bastion': {'Damage': 40, 'Mp': 30, 'Classe': 'Guerreiro', 'Efeito': 'Não sei'},
 }
