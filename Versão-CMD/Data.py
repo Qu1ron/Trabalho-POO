5dec22f65754b05a47cfcc4a290b91d7b393bb01
Lidrac <pedrohm3103@gmail.com>
2025-06-26 18:53:30 -0300

    Add files via upload

--- Versão-CMD/Data.py
+++ Versão-CMD/Data.py
@@ -1,14 +1,17 @@
+
+# Nesse arquivo temos uma biblioteca de ataques
+
 Ataques = {
     'Fireball': {'Damage': 25, 'Mp': 10, 'Classe': 'Mago', 'Efeito': 'burn'},
-    'Ice Spike': {'Damage': 20, 'Mp': 30, 'Classe': 'Mago', 'Efeito': 'freeze'},
+    'Ice Spike': {'Damage': 35, 'Mp': 25, 'Classe': 'Mago', 'Efeito': 'freeze'},
     'Thunderbolt': {'Damage': 30, 'Mp': 30, 'Classe': 'Mago', 'Efeito': 'shocked'},
     'Hydro Pump': {'Damage': 28, 'Mp': 20, 'Classe': 'Mago', 'Efeito': 'soaked'},
     
-    'Slash': {'Damage': 28, 'Mp': 20, 'Classe': 'Guerreiro', 'Efeito': 'bleed'},
+    'Slash': {'Damage': 32, 'Mp': 15, 'Classe': 'Guerreiro', 'Efeito': 'bleed'},
     'Blade of Fate': {'Damage': 50, 'Mp': 40, 'Classe': 'Guerreiro', 'Efeito': 'Não sei'},
     'Last Bastion': {'Damage': 40, 'Mp': 30, 'Classe': 'Guerreiro', 'Efeito': 'Não sei'},
 
-    'Double Shot' : { 'Damage': 24 ,'Mp': 15 , 'Classe' : 'Arqueiro' , 'Efeito' : ' ? ' },
+    'Double Shot' : { 'Damage': 28 ,'Mp': 12 , 'Classe' : 'Arqueiro' , 'Efeito' : ' ? ' },
     'Smoke Arrow' : { 'Damage': 30 ,'Mp': 20 , 'Classe' : 'Arqueiro' , 'Efeito' : ' Blinded ' },
     'Poison Arrow' : { 'Damage': 35 ,'Mp': 24 , 'Classe' : 'Arqueiro' , 'Efeito' : ' Poisoned ' },
 }
